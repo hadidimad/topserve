49b5bbd3ebfd3094cda2a3669e5e0cbe88285ac1
hadidimad <hadiidmad138@gmail.com>
2017-07-15 20:19:00 +0430

    first commit

--- Client/main.go
+++ Client/main.go
@@ -1,22 +1,257 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
 	"net"
-	"os"
+	"time"
+
+	//"os"
+	"encoding/json"
+	"reflect"
 )
 
+type handler func(v interface{}, unixTime int64)
+
+type Message struct {
+	Mode string
+	Data interface{}
+}
+
+type Client struct {
+	c               net.Conn
+	topic           map[string][]handler
+	YourPublishers  map[string]struct{}
+	YourSubscribers map[string]struct{}
+	Err             chan string
+}
+
+func (cl *Client) New(address string) (Client, error) {
+	c, err := net.Dial("tcp", address)
+	if err != nil {
+		return Client{}, err
+	}
+	return Client{
+		c:               c,
+		topic:           make(map[string][]handler),
+		YourPublishers:  map[string]struct{}{},
+		YourSubscribers: map[string]struct{}{},
+		Err:             make(chan string),
+	}, nil
+}
+
+func (cl *Client) Subscribe(name string, h handler) error {
+	for i := range cl.topic {
+		if i == name {
+			cl.topic[name] = append(cl.topic[name], h)
+			return nil
+		}
+	}
+	return fmt.Errorf("publisher not finded")
+}
+
+func (cl *Client) RegisterSubscriber(name string) error {
+	msg := Message{
+		Mode: "subscriber",
+		Data: name,
+	}
+	str, _ := json.Marshal(msg)
+	_, err := cl.c.Write([]byte(str))
+	if err != nil {
+		return err
+	}
+	Serr := <-cl.Err
+	if " " != Serr {
+		return fmt.Errorf(Serr)
+	}
+	cl.YourSubscribers[name] = struct{}{}
+	cl.topic[name] = []handler{}
+	return nil
+}
+
+func (cl *Client) DeregisterSubscriber(name string) error {
+	var finded bool
+	for i := range cl.YourSubscribers {
+		if i == name {
+			finded = true
+			break
+		}
+	}
+	if !finded {
+		return fmt.Errorf("you are not subscriber for %s", name)
+	}
+	msg := Message{
+		Mode: "delsubscriber",
+		Data: name,
+	}
+	str, _ := json.Marshal(msg)
+	_, err := cl.c.Write([]byte(str))
+	if err != nil {
+		return err
+	}
+	Serr := <-cl.Err
+	if " " != Serr {
+		return fmt.Errorf(Serr)
+	}
+	delete(cl.YourPublishers, name)
+	delete(cl.topic, name)
+	return nil
+}
+
+func (cl *Client) UnSubscribe(name string, h handler) error {
+	if cl.topic[name] == nil {
+		return fmt.Errorf("this publisher not found")
+	}
+	handlers := cl.topic[name]
+	address := reflect.ValueOf(h).Pointer()
+	index := -1
+	for i := range handlers {
+		if reflect.ValueOf(handlers[i]).Pointer() == address {
+			index = i
+			break
+		}
+	}
+	if index == -1 {
+		return fmt.Errorf("handler not found")
+	}
+	handlers = append(handlers[:index], handlers[index+1:]...)
+	cl.topic[name] = handlers
+	return nil
+}
+
+func (cl *Client) Publish(name string, v interface{}) error {
+	for i := range cl.topic {
+		if i == name {
+			handlers := cl.topic[name]
+			for _, i := range handlers {
+				go i(v, time.Now().Unix())
+			}
+		}
+	}
+	return nil
+}
+
+func (cl *Client) PublishServer(name string, v interface{}) error {
+	for i := range cl.YourPublishers {
+		if i == name {
+			var message Message
+			message.Mode = "publish"
+			m := make(map[string]interface{})
+			m["name"] = name
+			m["value"] = v
+			message.Data = m
+			JSONval, err := json.Marshal(message)
+			if err != nil {
+				return err
+			}
+			_, err = cl.c.Write(JSONval)
+			if err != nil {
+				return err
+			}
+			Serr := <-cl.Err
+			if " " != Serr {
+				return fmt.Errorf(Serr)
+			}
+			return nil
+		}
+	}
+	return fmt.Errorf("you have not registered this publisher")
+}
+
+func (cl *Client) RegisterPublisher(name string) error {
+	msg := Message{
+		Mode: "publisher",
+		Data: name,
+	}
+	str, _ := json.Marshal(msg)
+	_, err := cl.c.Write([]byte(str))
+	if err != nil {
+		return err
+	}
+	Serr := <-cl.Err
+	if " " != Serr {
+		return fmt.Errorf(Serr)
+	}
+
+	cl.YourPublishers[name] = struct{}{}
+	return nil
+}
+
+func (cl *Client) HandleIncomings() {
+	for {
+		msg := make([]byte, 256)
+		readLen, err := cl.c.Read(msg)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			var message Message
+			err = json.Unmarshal(msg[:readLen], &message)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				if message.Mode == "error" {
+					cl.Err <- message.Data.(string)
+				}
+				if message.Mode == "publish" {
+					Data := message.Data.(map[string]interface{})
+					err := cl.Publish(Data["name"].(string), Data["value"])
+					if err != nil {
+						fmt.Println(err)
+					}
+				}
+			}
+		}
+	}
+}
+
 func main() {
-	c, err := net.Dial("tcp", ":7777")
+	var client Client
+	client, err := client.New(":7777")
 	if err != nil {
 		fmt.Println(err)
 	}
+	go client.HandleIncomings()
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Enter text: ")
-		text, _ := reader.ReadString('\n')
-		fmt.Println(text)
-		c.Write([]byte(text))
+		var input string
+		fmt.Scan(&input)
+		if input == "rp" {
+			err := client.RegisterPublisher("test:test")
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		if input == "p" {
+			client.PublishServer("test:test", 23)
+		}
+		if input == "sub" {
+			err := client.Subscribe("test:test", func(v interface{}, unixTime int64) {
+				fmt.Println("test:test happens in ", unixTime, "and the value is ", v)
+			})
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		if input == "rsub" {
+			err := client.RegisterSubscriber("test:test")
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		if input == "drsub" {
+			err := client.DeregisterSubscriber("test:test")
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		if input == "usub" {
+			err := client.UnSubscribe("test:test", func(v interface{}, unixTime int64) {
+				fmt.Println("test:test happens in ", unixTime, "and the value is ", v)
+			})
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		fmt.Println("task Done")
 	}
+
 }
--- Server/main.go
+++ Server/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strings"
+	//"strings"
+	"encoding/json"
 )
 
 type Publisher struct {
-	name        string
+	Name        string
 	Conn        net.Conn
 	Subscribers map[*Subscriber]struct{}
 }
@@ -17,86 +18,229 @@ type Subscriber struct {
 	Conn net.Conn
 }
 
-type Topic struct {
+type Message struct {
+	Mode string
+	Data interface{}
+}
+
+type Server struct {
+	Listener   net.Listener
 	Publishers map[*Publisher]struct{}
 }
 
-func (t *Topic) RegisterPublisher(name string, conn net.Conn) error {
-	for i := range t.Publishers {
-		if i.name == name {
-			return fmt.Errorf("this name Registered Before")
-		}
-		if i.Conn == conn {
-			return fmt.Errorf("you registered a Publisher before")
+func (p *Publisher) RegisterSubscriber(c net.Conn) error {
+	for i := range p.Subscribers {
+		if c == i.Conn {
+			return fmt.Errorf("you are a subscriber for" + p.Name)
 		}
 	}
-	pub := &Publisher{
-		name:        name,
-		Conn:        conn,
-		Subscribers: map[*Subscriber]struct{}{},
+	sub := &Subscriber{
+		Conn: c,
 	}
-	t.Publishers[pub] = struct{}{}
+	p.Subscribers[sub] = struct{}{}
+	return nil
+}
+func (p *Publisher) Publish(v interface{}) error {
+	for o := range p.Subscribers {
+		var message Message
+		message.Mode = "publish"
+		m := make(map[string]interface{})
+		m["name"] = p.Name
+		m["value"] = v
+		message.Data = m
+		JSONval, err := json.Marshal(message)
+		if err != nil {
+			return err
+		}
+		_, err = o.Conn.Write(JSONval)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
 	return nil
 }
 
-func handleIncoming(c net.Conn, topic *Topic) {
+func (s *Server) New(address string) (Server, error) {
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		return Server{}, err
+	}
+	return Server{
+		Listener:   ln,
+		Publishers: map[*Publisher]struct{}{},
+	}, nil
+}
+
+func (s *Server) Publish(name string, v interface{}) error {
+	for o := range s.Publishers {
+		if o.Name == name {
+			err := o.Publish(v)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+	return fmt.Errorf("publisher not found")
+}
+
+func (s *Server) RegisterPublisher(name string, conn net.Conn) error {
+	for i := range s.Publishers {
+		if i.Name == name {
+			return fmt.Errorf("this name Registered Before")
+		}
+	}
+
+	pub := &Publisher{
+		Name:        name,
+		Conn:        conn,
+		Subscribers: map[*Subscriber]struct{}{},
+	}
+	s.Publishers[pub] = struct{}{}
+	return nil
+}
+
+func (s *Server) RegisterSubscriber(name string, c net.Conn) error {
+	for i := range s.Publishers {
+		if i.Name == name {
+			err := i.RegisterSubscriber(c)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+	return fmt.Errorf("publisher not found")
+}
+
+func (s *Server) DeRegisterSubscriber(name string, c net.Conn) error {
+	for i := range s.Publishers {
+		if i.Name == name {
+			for j := range i.Subscribers {
+				if j.Conn == c {
+					delete(i.Subscribers, j)
+					return nil
+				}
+			}
+			return fmt.Errorf("you are not subscriber for" + name)
+		}
+	}
+	return fmt.Errorf("publisher not found")
+}
+
+func (s *Server) AcceptConnection() (net.Conn, error) {
+	c, err := s.Listener.Accept()
+	if err != nil {
+		var t net.Conn
+		return t, err
+	}
+	return c, nil
+}
+
+func (s *Server) HandleConnection(c net.Conn) {
 	for {
-		msg := make([]byte, 128)
-		_, err := c.Read(msg)
+		msg := make([]byte, 256)
+		readLen, err := c.Read(msg)
 		if err != nil {
 			fmt.Println(err)
 			c.Close()
 			break
 		} else {
-			if strings.Contains(string(msg), "Register") {
-				///////register new Publisher part
-				if strings.Contains(string(msg), "Publisher") {
-					str := strings.Replace(string(msg), "Register", "", -1)
-					str = strings.Replace(str, "Publisher", "", -1)
-					for i := 0; i < len(str); i++ {
-						if str[i] == 0 {
-							str = str[0:i]
-							break
-						}
-					}
-					str = strings.Replace(str, " ", "", -1)
-					str = str[:len(str)-1]
-					err := topic.RegisterPublisher(str, c)
-					if err != nil {
-						c.Write([]byte(err.Error()))
-						fmt.Println("failed to Register" + str + "as Publsiher")
-					} else {
-						fmt.Println(str + "registered as Publisher")
-					}
-				}
-				//////register new subscriber
-				if strings.Contains(string(msg), "to") {
+			var message Message
+			var response Message
+			response.Mode = "error"
+			json.Unmarshal(msg[:readLen], &message)
+			if message.Mode == "publisher" {
+				err := s.RegisterPublisher(message.Data.(string), c)
+				if err != nil {
+					response.Data = err.Error()
+				} else {
+					response.Data = " "
+					fmt.Println("registered Publisher", message.Data.(string))
 
 				}
-			} else if strings.Contains(string(msg), "Deregister") {
-
+				b, _ := json.Marshal(response)
+				_, err = c.Write(b)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+			if message.Mode == "subscriber" {
+				err := s.RegisterSubscriber(message.Data.(string), c)
+				if err != nil {
+					response.Data = err.Error()
+				} else {
+					response.Data = " "
+					fmt.Println("registered subscriber", message.Data.(string))
+				}
+				b, _ := json.Marshal(response)
+				_, err = c.Write(b)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+			if message.Mode == "publish" {
+				Data := message.Data.(map[string]interface{})
+				fmt.Println(Data["name"], Data["value"])
+				err := s.Publish(Data["name"].(string), Data["value"])
+				if err != nil {
+					response.Data = err.Error()
+				} else {
+					response.Data = " "
+				}
+				b, _ := json.Marshal(response)
+				_, err = c.Write(b)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+			if message.Mode == "delsubscriber" {
+				err := s.DeRegisterSubscriber(message.Data.(string), c)
+				if err != nil {
+					response.Data = err.Error()
+				} else {
+					response.Data = " "
+				}
+				b, _ := json.Marshal(response)
+				_, err = c.Write(b)
+				if err != nil {
+					fmt.Println(err)
+				}
 			}
 		}
 	}
 }
 
 func main() {
-	ln, err := net.Listen("tcp", ":7777")
+	/*ln, err := net.Listen("tcp", ":7777")
 	if err != nil {
 		fmt.Println("err listen to port")
 		os.Exit(-1)
-	}
-	topic := Topic{
-		Publishers: map[*Publisher]struct{}{},
 	}
 	for {
 		c, err := ln.Accept()
 		if err != nil {
 			fmt.Println("error making connection")
 		}
-		go handleIncoming(c, &topic)
-		for i := range topic.Publishers {
+		c.Close()
+		//go handleIncoming(c, &topic)
+		/*for i := range topic.Publishers {
 			fmt.Println(i.name, i.Conn)
+		}
+	}*/
+	var server Server
+	server, err := server.New(":7777")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	for {
+		c, err := server.AcceptConnection()
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			go server.HandleConnection(c)
 		}
 	}
 }
